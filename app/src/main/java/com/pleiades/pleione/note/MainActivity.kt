bea0e05c7c8276459b791a363ce4d8a8d4dea511
Cluster Pleiades <Pleione.P.Cluster@gmail.com>
2022-08-30 17:57:51 +0900

    Set Gradle Dependency

--- app/src/main/java/com/pleiades/pleione/note/MainActivity.kt
+++ app/src/main/java/com/pleiades/pleione/note/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.pleiades.pleione.note
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.content.ContextCompat
 import com.pleiades.pleione.note.ui.theme.NoteTheme
 import com.pleiades.pleione.note.ui.theme.Purple
 
@@ -20,24 +14,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NoteTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = Purple) {
-                    Greeting("Android")
+//                    FloatingActionButton()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NoteTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun FloatingActionButton() {
+//    Scaffold {
+//        floatingActionButton = {
+//            FloatingActionButton(
+//                onClick = {}
+//            ) {
+//                Icon(Icons.Filled.Add,"")
+//            }
+//        }
+//    }
+//}
